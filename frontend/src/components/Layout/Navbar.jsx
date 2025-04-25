25eea1fbae3b9f6217d4b99ec1ea39dd819e106f
Sudhanshu Kumar <140949193+sudhanshu1512@users.noreply.github.com>
2025-04-25 17:34:51 +0530

    Update Navbar.jsx

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   const handleLogout = async () => {
     try {
       const response = await axios.get(
-        "http://localhost:3000/api/v1/user/logout",
+        "https://job-seeking-web-app-lmta.onrender.com/api/v1/user/logout",
         {
           withCredentials: true,
         }
